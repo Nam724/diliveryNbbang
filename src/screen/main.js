d8d3619945282c28aaddf0b213dc7f373a109a80
Nam724 <guardprec@gmail.com>
2022-05-19 18:12:49 +0900

    show user location by google maps

--- src/screen/main.js
+++ src/screen/main.js
@@ -1,15 +1,16 @@
 import {View, Text} from 'react-native';
 import {useState, useEffect} from 'react';
-
 import { styles, width } from '../style/style';
 
 import * as Location from 'expo-location';
-import WebView from 'react-native-webview';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function Main() {
     // get location
-    const [location, setLocation] = useState(null); // {latitude: 37.4219525, longitude: -122.0837251}
+    const [location, setLocation] = useState(
+      {latitude: 37.4219525, longitude: -122.0837251, latitudeDelta: 0.0922, longitudeDelta: 0.0421}
+    ); // {latitude: 37.4219525, longitude: -122.0837251}
     const [errorMsg, setErrorMsg] = useState(null);
 
     useEffect(() => {
@@ -22,8 +23,10 @@ export default function Main() {
     
           let location = await Location.getCurrentPositionAsync({});
           let _locationdict = {
-            'latitude': location.coords.latitude,
-            'longitude': location.coords.longitude,
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
           }
           setLocation(_locationdict);
         })();
@@ -35,19 +38,35 @@ export default function Main() {
       } else if (location) {
         text = JSON.stringify(location);
       }
+  console.log(location)
+
+  // move maps
 
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.titleText}>MY APPLICATION</Text>
-        </View>
-        <View>
-            <MapView
-                style={styles.map}
-                initialRegion={{ latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421, }}
+      <View style={styles.header}>
+          <Text style={styles.titleText}>MY APPLICATION</Text>
+      </View>
+
+      <View>
+        {location && (
+          <MapView
+          provider='google'
+          style={styles.map}
+          initialRegion={location}
+          showsMyLocationButton={true}
+          showsUserLocation={true}
+          loadingEnabled={true}
+          zoomEnabled={true}
+          rotateEnabled={true}
+          >
+            <Marker
+            coordinate={location}
             />
-        </View>
-        
+          </MapView>
+        )}
+      </View>
+
     </View>
     );  // return
 }
