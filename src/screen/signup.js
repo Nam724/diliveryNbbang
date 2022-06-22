8179954f228a069142e6a8c3eaae6c41492a4517
Nam724 <guardprec@gmail.com>
2022-06-22 18:50:22 +0900

    verification code completed

--- src/screen/signup.js
+++ src/screen/signup.js
@@ -3,20 +3,20 @@ import { useState } from 'react';
 import { TextInput, TouchableOpacity, View, Text } from 'react-native';
 import { styles } from '../style/style';
 
-async function signUp(email, password) {
-    const username = email;
-    const phone_number = '';
+async function signUp(email, password, setVerification_code_sended) {
+    
     try {
         const { user } = await Auth.signUp({
-            username: username,
+            username: email,
             password: password,
             attributes: {
                 email:email,          // optional
-                phone_number,   // optional - E.164 number convention
+                phone_number:'',   // optional - E.164 number convention
                 // other custom attributes 
             }
         });
         console.log(user);
+        setVerification_code_sended(true);
     } catch (error) {
         console.log('error signing up:', error);
     }
@@ -32,13 +32,25 @@ function emailTest(email){
     }
 }
 
-export default function Signup_page(){
+async function confirmSignUp(username, code) {
+    try {
+      await Auth.confirmSignUp(username, code);
+      return(true);
+    } catch (error) {
+        console.log('error confirming sign up', error);
+    }
+}
+
+export default function SignUp_page(){
     const [email, setEmail] = useState('');
     const [isEmailValid, setIsEmailValid] = useState(false);
     const [password, setPassword] = useState('');
+    const [verification_code, setVerification_code] = useState('');
+    const [verification_code_sended, setVerification_code_sended] = useState(false);
+
     return(
         <View style={[styles.container,{
-            paddingTop:100,
+            paddingTop:200,
         }]}>
             <View style={styles.header}>
             <View>
@@ -71,12 +83,42 @@ export default function Signup_page(){
                         borderWidth: 1,
                         height:40,
                     }}
+                    maxLength={20}
                     onChangeText={(password) => setPassword(password)}
+                />
+            </View>
+            <View>
+               
+                <TouchableOpacity
+                onPressOut={() => signUp(email, password, setVerification_code_sended)}
+                disabled={!email}
+                >
+                <Text style={styles.titleText}>
+                {'send verification code(email)'}
+                </Text>
+            </TouchableOpacity>
+            </View>
+            <View style>
+               <Text style={styles.titleText}>
+                {!verification_code_sended?'send verification code first':'verification code'}
+                </Text>
+                <TextInput 
+                    autoComplete='password'
+                    keyboardType='default'
+                    style={{
+                        borderColor:'red',
+                        borderWidth: 1,
+                        height:40,
+                    }}
+                    maxLength={6}
+                    onChangeText={(verification_code) => setVerification_code(verification_code)}
                 />
             </View>
                 
             <TouchableOpacity
-                onPressOut={() => signUp(email, password)}
+                onPressOut={() => confirmSignUp(email, verification_code)
+                }
+                disabled={!verification_code}
             >
                 <Text style={styles.titleText}>
                 Sign Up
