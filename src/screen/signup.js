da899ddc4a73ac19b205747b7b66d594ab3902ae
Nam724 <guardprec@gmail.com>
2022-07-06 18:22:58 +0900

    문자메세지 인증 코드로 변경

--- src/screen/signup.js
+++ src/screen/signup.js
@@ -3,36 +3,36 @@ import { useEffect, useState } from 'react';
 import { TextInput, TouchableOpacity, View, Text } from 'react-native';
 import { styles, height, width, colorPack } from '../style/style';
 
-async function sendVerificationCode(email, password, setVerification_code_sended) {
+// async function sendVerificationCode(email, password, setVerification_code_sended) {
     
-    try {
-        const { user } = await Auth.signUp({
-            username: email,
-            password: password,
-            attributes: {
-                email:email,          // optional
-                phone_number:'',   // optional - E.164 number convention
-                // other custom attributes 
-            }
-        });
-        console.log(user);
-        setVerification_code_sended(true);
-    } catch (error) {
-        console.log('error signing up:', error);
-        if(error.code === 'UsernameExistsException'){
-            alert('User already exists');
-            return(false);
-        }
-        else if(error.code === 'InvalidParameterException'){
-            alert('Invalid parameter');
-            return(false);
-        }
-        else if (error.code === 'NetworkError'){
-            alert('Network error');
-            return(false);
-        }
-    }
-}
+//     try {
+//         const { user } = await Auth.signUp({
+//             username: email,
+//             password: password,
+//             attributes: {
+//                 email:email,          // optional
+//                 phone_number:'',   // optional - E.164 number convention
+//                 // other custom attributes 
+//             }
+//         });
+//         console.log(user);
+//         setVerification_code_sended(true);
+//     } catch (error) {
+//         console.log('error signing up:', error);
+//         if(error.code === 'UsernameExistsException'){
+//             alert('User already exists');
+//             return(false);
+//         }
+//         else if(error.code === 'InvalidParameterException'){
+//             alert('Invalid parameter');
+//             return(false);
+//         }
+//         else if (error.code === 'NetworkError'){
+//             alert('Network error');
+//             return(false);
+//         }
+//     }
+// }
 
 function emailTest(setEmail, email){
     const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -45,28 +45,11 @@ function emailTest(setEmail, email){
     }
 }
 
-async function confirmSignUp(username, code, navigation) {
-    try {
-      await Auth.confirmSignUp(username, code);
-      alert('회원가입이 완료되었습니다.');
-      navigation.navigate('SignIn');
-      return(true);
-    } catch (error) {
-        console.log('error confirming sign up', error);
-        if (error === 'NetworkError'){
-            alert('Network error');
-            return(false);
-        }
-        else if (error === 'CodeMismatchException'){
-            alert('잘못된 코드입니다.');
-            return(false);
-        }
-    }
-}
 
 export default function SignUp_page({navigation}){
     const [email, setEmail] = useState('');
     const [isEmailValid, setIsEmailValid] = useState(false);
+    const [phoneNumber , setPhoneNumber] = useState('');
     const [password, setPassword] = useState('');
     const [verification_code, setVerification_code] = useState('');
     const [verification_code_sended, setVerification_code_sended] = useState(false);
@@ -85,6 +68,61 @@ export default function SignUp_page({navigation}){
             setSendVerificationCodeBtn(false);
         }
     },[verification_code_sended, password, email]);
+
+
+    const sendVerificationCode= async() =>{
+    
+        try {
+            const { user } = await Auth.signUp({
+                username: email,
+                password: password,
+                attributes: {
+                    email:email,          // optional
+                    phone_number:'+82'+phoneNumber.substring(1),   // optional - E.164 number convention
+                    // other custom attributes 
+                }
+            });
+            console.log(user);
+            setVerification_code_sended(true);
+        } catch (error) {
+            console.log('error signing up:', error);
+            if(error.code === 'UsernameExistsException'){
+                alert('User already exists');
+                return(false);
+            }
+            else if(error.code === 'InvalidParameterException'){
+                alert('Invalid parameter');
+                return(false);
+            }
+            else if (error.code === 'NetworkError'){
+                alert('Network error');
+                return(false);
+            }
+        }
+    }
+
+    const confirmSignUp = async() => {
+        try {
+          await Auth.confirmSignUp(email, verification_code);
+          alert('회원가입이 완료되었습니다.');
+          navigation.navigate('SignIn');
+          return(true);
+        } catch (error) {
+            console.log('error confirming sign up', error);
+            if (error === 'NetworkError'){
+                alert('Network error');
+                return(false);
+            }
+            else if (error === 'CodeMismatchException'){
+                alert('잘못된 코드입니다.');
+                return(false);
+            }
+        }
+    }
+    
+
+
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
@@ -92,9 +130,9 @@ export default function SignUp_page({navigation}){
                     {'My Application 회원가입'}
                 </Text>            
             </View>
-            <View style={{marginTop:height*249/2000, height:height*179/2000}}>
+            <View style={{marginTop:height*50/2000, height:height*179/2000}}>
                 <Text style={styles.highlightText}>
-                Email
+                {'아이디(이메일)'}
                 </Text>
                 <TextInput 
                     keyboardType='email-address'
@@ -104,9 +142,23 @@ export default function SignUp_page({navigation}){
                     }}
                 />
             </View>
-            <View style={{marginTop: height*100/2000,height:height*179/2000}}>
+            <View style={{marginTop:height*50/2000, height:height*179/2000}}>
                 <Text style={styles.highlightText}>
-                Password
+                전화번호
+                </Text>
+                <TextInput 
+                    keyboardType='numeric'
+                    style={[styles.textInputBox, styles.normalText]}
+                    onChangeText={(num) => {
+                        setPhoneNumber(num)
+                    }}
+                    placeholderTextColor={colorPack.deactivated}
+                    placeholder={'01012345678'}
+                />
+            </View>
+            <View style={{marginTop: height*50/2000,height:height*179/2000}}>
+                <Text style={styles.highlightText}>
+                비밀번호
                 </Text>
                 <TextInput 
                     secureTextEntry={true}
@@ -120,12 +172,12 @@ export default function SignUp_page({navigation}){
             <View>
                
                 <TouchableOpacity
-                onPressOut={() => sendVerificationCode(email, password, setVerification_code_sended)}
+                onPressOut={() => sendVerificationCode()}
                 disabled={!sendVerificationCodeBtn}
                 style={[styles.goToSignUpInButton, {marginTop:height*100/2000}]}
                 >
                 <Text style={sendVerificationCodeBtn?styles.highlightText:styles.deactivatedText}>
-                {!verification_code_sended?'인증코드 보내기(이메일)':'인증코드를 입력하세요'}
+                {!verification_code_sended?'인증코드 보내기(문자)':'인증코드를 입력하세요'}
                 </Text>
                 </TouchableOpacity>
             </View>
@@ -137,7 +189,7 @@ export default function SignUp_page({navigation}){
                     maxLength={6}
                     onChangeText={(verification_code) => {setVerification_code(verification_code)
                     if (verification_code.length === 6){
-                        confirmSignUp(email, verification_code, navigation);
+                        confirmSignUp();
                         }
                     }
                 }   
@@ -145,7 +197,7 @@ export default function SignUp_page({navigation}){
             </View>
                 
             <TouchableOpacity
-                onPressOut={() => confirmSignUp(email, verification_code, navigation)
+                onPressOut={() => confirmSignUp()
                 }
                 disabled={!verification_code}
                 style={[styles.goToSignUpInButton, {marginTop:height*100/2000}]}
