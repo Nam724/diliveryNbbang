cbe56043156a1d24d8abfe47e7db56c04326adb8
Nam724 <guardprec@gmail.com>
2022-07-10 01:36:52 +0900

    로딩 페이지 디자인 변경

--- src/screen/loading_page.js
+++ src/screen/loading_page.js
@@ -1,5 +1,5 @@
 import { colorPack, styles, width } from '../style/style';
-import {View, ActivityIndicator, Text} from 'react-native';
+import {View, ActivityIndicator, Text, Image} from 'react-native';
 
 
 
@@ -7,10 +7,22 @@ export default function Loading_page(){
   
   return ( 
       <View style={[styles.container, {flex:1, justifyContent:'center'}]}>
-      <Text style={styles.highlightText}>
-        {'정보를 업데이트 하는 중이예요.'}
-      </Text>
-        <ActivityIndicator size="large" animating={true} color={colorPack.highlight_dark} />
+
+
+      <View style={{alignContent:'center'}}>
+        <Image source={require('../../assets/icon.png')} style={{width:width*0.3, height:width*0.3, marginLeft:width*0.35}} />
+      </View>
+
+
+      <ActivityIndicator size="large" animating={true} color={colorPack.text_light} />
+
+      <View style={styles.header}>
+        <Text style={styles.highlightText}>
+            {'정보를 불러오는 중이예요'}
+        </Text>
+      </View>
+
+
       </View>
   );
 }
