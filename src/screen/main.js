ef1bf7341f4f48246512fa2a7a59b614daa34626
Nam724 <guardprec@gmail.com>
2022-07-12 04:20:15 +0900

    isLoading을 없애보았습니다.

--- src/screen/main.js
+++ src/screen/main.js
@@ -362,30 +362,7 @@ const restaurantList_sample = [
 
  // return 
   return (
-    (isLoading)?
-      (
-
-        <View style={[styles.container, {flex:1, justifyContent:'center'}]}>
-
-
-        <View style={{alignContent:'center'}}>
-          <Image source={require('../../assets/icon.png')} style={{width:width*0.3, height:width*0.3, marginLeft:width*0.35}} />
-        </View>
-
-
-        <ActivityIndicator size="large" animating={true} color={colorPack.text_light} />
-
-        <View style={styles.header}>
-          <Text style={styles.highlightText}>
-              {'정보를 불러오는 중이예요'}
-          </Text>
-        </View>
-
-
-        </View>
-
-      ):
-    (<View style={styles.container}>
+    <View style={styles.container}>
 
       <DialogInput
         isDialogVisible={dialogVisible_marker}
@@ -722,7 +699,7 @@ const restaurantList_sample = [
 
 
       </View>
-    </View>)
+    </View>
   )
 
   }// return}
