29a5e2b75f2e40df21f440b4ac92606615079ef2
Nam724 <guardprec@gmail.com>
2022-07-12 21:22:17 +0900

    로딩창이 변경되었고, 로딩창이 바꼈습니다.(splah changed)

--- src/screen/loading_page.js
+++ src/screen/loading_page.js
@@ -9,16 +9,16 @@ export default function Loading_page({route, navigation}){
 
 
   return ( 
-      <View style={[styles.container, {flex:1, justifyContent:'center'}]}>
+      <View style={[styles.container, {justifyContent:'center'}]}>
 
 
-      <View style={{alignContent:'center', width:width}}>
-        <Image source={require('../../assets/icon.png')} style={{width:width*0.5, height:width*0.5,}} />
+      <View style={{alignContent:'center', justifyContent:'center'}}>
+        <Image source={require('../../assets/icon.png')} style={{width:width*0.8, height:width*0.8, marginLeft:width*0.1}} />
       </View>
 
       <View style={styles.header}>
       <Text style={styles.highlightText}>
-          {'배달 앤빵'}
+          {'배달 앤빵 정보를 불러오는 중입니다.'}
       </Text>
     </View>
 
