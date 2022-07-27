000492e6a3d2e4892234db39b173ca3876be9b64
Nam724 <guardprec@gmail.com>
2022-07-27 19:19:31 +0900

    webpack.config.js 수정

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -4,8 +4,8 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // If you want to add a new alias to the config.
-  config.resolve.alias['react-native'] = 'react-native-web';
-  config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+  config.config.resolve.alias['react-native'] = 'react-native-web';
+  config.config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
 
   // Maybe you want to turn off compression in dev mode.
   if (config.mode === 'development') {
