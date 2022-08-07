24b23aee7dfa55d2bbdf8ada2bd976c39eacf319
Nam724 <guardprec@gmail.com>
2022-08-07 23:01:28 +0900

    아이폰에서 광고 사용 요청

--- utils/Ads.js
+++ utils/Ads.js
@@ -1,12 +1,46 @@
+import {
+    AdMobBanner,
+    AdMobInterstitial,
+    PublisherBanner,
+    AdMobRewarded,
+    setTestDeviceIDAsync,
+    requestPermissionsAsync,
+} from "expo-ads-admob";
+import { useEffect, useState } from "react";
 import { Platform, View } from "react-native";
 import { styles } from "../src/style/style";
 
-const isAdsEnabled =
-    Platform.OS === "android" || Platform.OS === "ios";
+const adBannerUnitId =
+    Platform.OS === "android"
+        ? "ca-app-pub-1145139773627965/8701930912"
+        : "ca-app-pub-3940256099942544/2934735716"; // 샘플 광고 ID
 
 export function RestaurantBannerAds() {
+    useEffect(() => {
+        getPermissions();
+    }, []); // 안드로이드 또는 아이폰일 경우 광고 사용 권한 요청
+    const [isAdsEnabled, setIsAdsEnabled] = useState(
+        Platform.OS === "android" || Platform.OS === "ios"
+    );
+    const getPermissions = async () => {
+        let { status } = true;
+        if (Platform.OS === "ios") {
+            status = await requestPermissionsAsync();
+        }
+        setIsAdsEnabled(true);
+    };
+
     return isAdsEnabled ? (
-        <View style={styles.adsContainer}></View>
+        <View style={styles.adsContainer}>
+            <AdMobBanner
+                bannerSize="banner"
+                adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
+                servePersonalizedAds={true} // true or false
+                onDidFailToReceiveAdWithError={(err) => {
+                    console.log(err);
+                }}
+            />
+        </View>
     ) : (
         <View></View>
     );
