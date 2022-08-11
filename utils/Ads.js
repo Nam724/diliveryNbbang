9983526b436de1f06431d7da0ae6d3685d127138
Nam724 <guardprec@gmail.com>
2022-08-11 22:11:11 +0900

    메인 페이지 광고에서만 앱에서 추적 금지 요청이 뜹니다.

--- utils/Ads.js
+++ utils/Ads.js
@@ -1,43 +1,157 @@
 import {
     AdMobBanner,
-    AdMobInterstitial,
-    setTestDeviceIDAsync,
     requestPermissionsAsync,
     getPermissionsAsync,
 } from "expo-ads-admob";
 import { useEffect, useState } from "react";
-import { Alert, Platform, View } from "react-native";
+import {
+    Alert,
+    Linking,
+    Platform,
+    View,
+} from "react-native";
 import { styles } from "../src/style/style";
 
-const adBannerUnitId = Platform.select({
-    ios: "ca-app-pub-1145139773627965/9125452802",
-    android: "ca-app-pub-1145139773627965/8701930912",
-});
+let servePersonalizedAds = true;
+let isAdsEnabled =
+    Platform.OS === "android" || Platform.OS === "ios";
+export function MainBannerAds() {
+    const adBannerUnitId = Platform.select({
+        ios: "ca-app-pub-1145139773627965/9125452802",
+        android: "ca-app-pub-1145139773627965/8701930912",
+    });
 
-export function RestaurantBannerAds() {
     useEffect(() => {
-        getPermissions();
-    }, []); // 안드로이드 또는 아이폰일 경우 광고 사용 권한 요청
-    const [isAdsEnabled, setIsAdsEnabled] = useState(
-        Platform.OS === "android" || Platform.OS === "ios"
-    );
-    const getPermissions = async () => {
-        let { status } = true;
-        await setTestDeviceIDAsync("EMULATOR");
-        await AdMobInterstitial.setAdUnitID(adBannerUnitId); // Test ID, Replace with your-admob-unit-id
-        await AdMobInterstitial.requestAdAsync({
-            servePersonalizedAds: true,
-        });
-        await AdMobInterstitial.showAdAsync();
-        setIsAdsEnabled(true);
-    };
+        async () => {
+            let { status } = await getPermissionsAsync();
+            if (status !== "granted") {
+                let { statusRequested } =
+                    await requestPermissionsAsync();
+                if (statusRequested === "granted") {
+                    isAdsEnabled = true;
+                } else {
+                    Alert.alert(
+                        "배달앤빵",
+                        "앱이 광고를 사용하기 위해서는 권한이 필요합니다.\n설정에서 권한을 허용해주세요.",
+                        [
+                            {
+                                text: "확인",
+                                onPress: () => {
+                                    Linking.openSettings();
+                                },
+                            },
+                            {
+                                text: "취소",
+                                onPress: () => {
+                                    servePersonalizedAds = false;
+                                },
+                            },
+                        ]
+                    );
+                }
+            } else {
+                isAdsEnabled = true;
+            }
+        };
+    });
 
     return isAdsEnabled ? (
         <View style={styles.adsContainer}>
             <AdMobBanner
                 bannerSize="banner"
                 adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
-                servePersonalizedAds={true} // true or false
+                servePersonalizedAds={servePersonalizedAds} // true or false
+                onDidFailToReceiveAdWithError={(err) => {
+                    setIsAdsEnabled(false);
+                    alert(err);
+                }}
+            />
+        </View>
+    ) : (
+        <View></View>
+    );
+}
+
+export function RestaurantAuthBannerAds() {
+    const adBannerUnitId = Platform.select({
+        ios: "ca-app-pub-1145139773627965/5219336115",
+        android: "ca-app-pub-1145139773627965/9836140498",
+    });
+
+    return isAdsEnabled ? (
+        <View style={styles.adsContainer}>
+            <AdMobBanner
+                bannerSize="banner"
+                adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
+                servePersonalizedAds={servePersonalizedAds} // true or false
+                onDidFailToReceiveAdWithError={(err) => {
+                    setIsAdsEnabled(false);
+                    alert(err);
+                }}
+            />
+        </View>
+    ) : (
+        <View></View>
+    );
+}
+
+export function RestaurantGuestBannerAds() {
+    const adBannerUnitId = Platform.select({
+        ios: "ca-app-pub-1145139773627965/9825080947",
+        android: "ca-app-pub-1145139773627965/1391771748",
+    });
+    return isAdsEnabled ? (
+        <View style={styles.adsContainer}>
+            <AdMobBanner
+                bannerSize="banner"
+                adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
+                servePersonalizedAds={servePersonalizedAds} // true or false
+                onDidFailToReceiveAdWithError={(err) => {
+                    setIsAdsEnabled(false);
+                    alert(err);
+                }}
+            />
+        </View>
+    ) : (
+        <View></View>
+    );
+}
+
+export function RestaurantFinishedBannerAds() {
+    const adBannerUnitId = Platform.select({
+        ios: "ca-app-pub-1145139773627965/7462356079",
+        android: "ca-app-pub-1145139773627965/9078690071",
+    });
+
+    return isAdsEnabled ? (
+        <View style={styles.adsContainer}>
+            <AdMobBanner
+                bannerSize="banner"
+                adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
+                servePersonalizedAds={servePersonalizedAds} // true or false
+                onDidFailToReceiveAdWithError={(err) => {
+                    setIsAdsEnabled(false);
+                    alert(err);
+                }}
+            />
+        </View>
+    ) : (
+        <View></View>
+    );
+}
+
+export function ChatBannerAds() {
+    const adBannerUnitId = Platform.select({
+        ios: "ca-app-pub-1145139773627965/7665821460",
+        android: "ca-app-pub-1145139773627965/1471662793",
+    });
+
+    return isAdsEnabled ? (
+        <View style={styles.adsContainer}>
+            <AdMobBanner
+                bannerSize="banner"
+                adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
+                servePersonalizedAds={servePersonalizedAds} // true or false
                 onDidFailToReceiveAdWithError={(err) => {
                     setIsAdsEnabled(false);
                     alert(err);
